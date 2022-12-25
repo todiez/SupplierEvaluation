a79ffb8533a85b7d51c964b0b418efaeb23bfb36
tybo-dev <tobias@diez.guru>
2022-12-25 11:51:02 +0100

    input form adjusted

--- src/Home.js
+++ src/Home.js
@@ -1,108 +1,189 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [title, setTitle] = useState(["score", 0, 5, 10, 15, 20]);
-  const [body, setBody] = useState("");
-  const [score, setScore] = useState("Paz");
   const [isPending, setIsPending] = useState(false);
 
+  const [priceBoundary1, setPriceBoundary1] = useState(2.1);
+  const [priceBoundary2, setPriceBoundary2] = useState(0.3);
+  const [priceBoundary3, setPriceBoundary3] = useState(-0.3);
+  const [priceBoundary4, setPriceBoundary4] = useState(-2.1);
+
+  const [otdBoundary1, setOtdBoundary1] = useState(95);
+  const [otdBoundary2, setOtdBoundary2] = useState(90);
+  const [otdBoundary3, setOtdBoundary3] = useState(75);
+  const [otdBoundary4, setOtdBoundary4] = useState(60);
+
+  const [qBoundary1, setQBoundary1] = useState(98);
+  const [qBoundary2, setQBoundary2] = useState(95);
+  const [qBoundary3, setQBoundary3] = useState(92);
+  const [qBoundary4, setQBoundary4] = useState(90);
+
+  const [score1, setScore1] = useState(0);
+  const [score2, setScore2] = useState(5);
+  const [score3, setScore3] = useState(10);
+  const [score4, setScore4] = useState(15);
+  const [score5, setScore5] = useState(20);
+
   const handleSubmit = (e) => {
-    e.preventDefault();
-    const blog = { title, body, score };
-    setIsPending(true);
+    // e.preventDefault();
+    // const blog = { title, body, score };
+    // setIsPending(true);
   };
 
-  let scoreArray = ["score", 0, 5, 10, 15, 20];
-  let priceArray = ["price", 2, 0.3, -0.3, -2];
-  let otdArray = ["otd", 95, 90, 75, 60];
-  let qualityArray = ["quality", 98, 95, 90];
-
-  let Array = [];
-  Array.push(scoreArray);
-  Array.push(priceArray);
-  Array.push(otdArray);
-  Array.push(qualityArray);
-
-  console.log(Array);
+ 
 
   //Link to percentage input
   //https://codepen.io/tomasMetcalfe/pen/poojJLE
   return (
     <div className="home">
       <div className="container text-center">
-        <div className="row align-items-start">
+        <div className="row align-item-start">
           <div className="col">
-            Current achievable scores per catergory: {scoreArray}
-            <br />
-            Current boundaries in % for price changes: {priceArray}
-            <br />
-            Current boundaries for OnTime Delivery: {otdArray}
-            <br />
-            Current boundaries for Quality: {qualityArray}
-          </div>
-
-          <div className="col">
-            <form>
-              <label>Score:</label>
-              <label>Price Boundary 1</label> 
-               <input
+          <form>
+              <label>Max Score</label>
+              <input
                 type="number"
                 required
-                value={priceArray[1]}
-                onChange={(e) => setTitle(e.target.value)} //makes changes in the input field possible
+                value={score5}
+                onChange={(e) => setScore5(e.target.value)} //makes changes in the input field possible
               />
-
-
-
-              <label>Price Boundary 2</label>
-              <input type="number"></input>
-              <label>Price Boundary 3</label>
-              <input type="number"></input>
-              <label>Price Boundary 4</label>
-              <input type="number"></input>
+              <label>Score 4</label>
+              <input
+                type="number"
+                required
+                value={score4}
+                onChange={(e) => setScore4(e.target.value)} //makes changes in the input field possible
+              />
+              <label>Score 3</label>
+              <input
+                type="number"
+                required
+                value={score3}
+                onChange={(e) => setScore3(e.target.value)} //makes changes in the input field possible
+              />
+                   <label>Score 2</label>
+              <input
+                type="number"
+                required
+                value={score2}
+                onChange={(e) => setScore2(e.target.value)} //makes changes in the input field possible
+              />
+              <label>Min Score</label>
+              <input
+                type="number"
+                required
+                value={score1}
+                onChange={(e) => setScore1(e.target.value)} //makes changes in the input field possible
+              />
             </form>
           </div>
 
           <div className="col">
             <form>
-              <label>Score:</label>
-              <select value={score} onChange={(e) => setScore(e.target.value)}>
-                <option value="0">0</option>
-                <option value="5">5</option>
-                <option value="10">10</option>
-                <option value="15">15</option>
-                <option value="20">20</option>
-              </select>
+              <br />
+              <label>Price Boundary 1</label>
+              <input
+                type="number"
+                required
+                value={priceBoundary4}
+                onChange={(e) => setPriceBoundary4(e.target.value)} //makes changes in the input field possible
+              />
+              <label>Price Boundary 2</label>
+              <input
+                type="number"
+                required
+                value={priceBoundary3}
+                onChange={(e) => setPriceBoundary3(e.target.value)} //makes changes in the input field possible
+              />
+              <label>Price Boundary 3</label>
+              <input
+                type="number"
+                required
+                value={priceBoundary2}
+                onChange={(e) => setPriceBoundary2(e.target.value)} //makes changes in the input field possible
+              />
+              <label>Price Boundary 4</label>
+              <input
+                type="number"
+                required
+                value={priceBoundary1}
+                onChange={(e) => setPriceBoundary1(e.target.value)} //makes changes in the input field possible
+              />
             </form>
           </div>
 
           <div className="col">
-            <form onSubmit={handleSubmit}>
-              <label>Blog Title:</label>
+            <form>
+            <br />
+              <label>OTD Boundary 1</label>
               <input
-                type="text"
+                type="number"
                 required
-                value={title}
-                onChange={(e) => setTitle(e.target.value)} //makes changes in the input field possible
+                value={otdBoundary1}
+                onChange={(e) => setOtdBoundary1(e.target.value)} //makes changes in the input field possible
               />
-              <label>Blog Body:</label>
-              <textarea
+              <label>OTD Boundary 2</label>
+              <input
+                type="number"
                 required
-                value={body}
-                onChange={(e) => setBody(e.target.value)} //makes changes in the input field possible
-              ></textarea>
-              <label>Blog Author:</label>
-              {/* <select
-                value={author}
-                onChange={(e) => setAuthor(e.target.value)}
-              >
-                <option value="Paz">Paz</option>
-                <option value="Ida">Ida</option>
-              </select> */}
-              {!isPending && <button>Add Blog</button>}
-              {isPending && <button disabled>Adding Blog...</button>}
+                value={otdBoundary2}
+                onChange={(e) => setOtdBoundary2(e.target.value)} //makes changes in the input field possible
+              />
+              <label>OTD Boundary 3</label>
+              <input
+                type="number"
+                required
+                value={otdBoundary3}
+                onChange={(e) => setOtdBoundary3(e.target.value)} //makes changes in the input field possible
+              />
+              <label>OTD Boundary 4</label>
+              <input
+                type="number"
+                required
+                value={otdBoundary4}
+                onChange={(e) => setOtdBoundary4(e.target.value)} //makes changes in the input field possible
+              />
             </form>
           </div>
+
+          <div className="col">
+            <form>
+            <br />
+              <label>Quality Boundary 1</label>
+              <input
+                type="number"
+                required
+                value={qBoundary1}
+                onChange={(e) => setQBoundary1(e.target.value)} //makes changes in the input field possible
+              />
+              <label>Quality Boundary 2</label>
+              <input
+                type="number"
+                required
+                value={qBoundary2}
+                onChange={(e) => setQBoundary2(e.target.value)} //makes changes in the input field possible
+              />
+              <label>Quality Boundary 3</label>
+              <input
+                type="number"
+                required
+                value={qBoundary3}
+                onChange={(e) => setQBoundary3(e.target.value)} //makes changes in the input field possible
+              />
+              <label>Quality Boundary 4</label>
+              <input
+                type="number"
+                required
+                value={qBoundary4}
+                onChange={(e) => setQBoundary4(e.target.value)} //makes changes in the input field possible
+              />
+            </form>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <br /> <br /> <br />
+            {!isPending && <button>Adjust Values</button>}
+            {isPending && <button disabled>Adjusting Valuess...</button>}
+          </form>
         </div>
       </div>
     </div>
